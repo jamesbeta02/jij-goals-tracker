e63a71379e424917c235e7754725f88e2a42b25e
jamesbeta02 <jamesdbeta02@gmail.com>
2025-08-28 11:42:55 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2mm0WC9nyVtzu5Jk_UTbK6bW8emv322I",
+  authDomain: "jij-goal-tracker.firebaseapp.com",
+  projectId: "jij-goal-tracker",
+  storageBucket: "jij-goal-tracker.firebasestorage.app",
+  messagingSenderId: "1038300694803",
+  appId: "1:1038300694803:web:acf48edbe5d387545372e6"
+};
+
+
+const app = initializeApp(firebaseConfig);
